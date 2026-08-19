307e6522f5751e5c94eebd2c6832726cdcc1de91
AmossLee274 <ofahengauekingzton7@gmail.com>
2026-08-20 10:58:55 +1200

    Just a bit of tidying up

--- main.gd
+++ main.gd
@@ -1,37 +1,51 @@
 extends Node2D
 
-
+var points = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+
+# Top pipe codes.
 	if $Straight.left and $Straight2.right == true:
-		print("connected")
+		print("Top horizontal")
 	if $Straight.right and $Straight2.left == true:
-		print("connected")
+		print("Top Horizonal")
 
-	if $Straight.up and $Straight2.down == true:
-		print("not connected") 
-	if $Straight.down and $Straight2.up == true:
-		print("not connected")
+# Left two pipe codes.
+	if $Straight.down and $Straight3.up == true:
+		print("Left Vertical") 
+	if $Straight.up and $Straight3.down == true:
+		print("Left Vertical") 
 
-	if $Straight.left and $Straight2.up == true:
-		print("not connected")
-	if $Straight.left and $Straight2.down == true:
-		print("not connected")
-	if $Straight.right and $Straight2.up == true:
-		print("not connected")
-	if $Straight.right and $Straight2.down == true:
-		print("not connected")
+# Right two pipe codes.
+	if $Straight2.up and $Straight4.down == true:
+		print("Right Vertical") 
+	if $Straight2.down and $Straight4.up == true:
+		print("Right Vertical") 
 
-	if $Straight.up and $Straight2.left == true:
-		print("not connected")
-	if $Straight.up and $Straight2.right == true:
-		print("not connected")
-	if $Straight.down and $Straight2.left == true:
-		print("not connected")
-	if $Straight.down and $Straight2.right == true:
-		print("not connected")
+# Bottom two pipe codes.
+	if $Straight3.left and $Straight4.right == true:
+		print("Bottom Horizontal")
+	if $Straight3.right and $Straight4.left == true:
+		print("Bottom Horizontal")
+
+
+# All disconnected.
+	if $Straight.down and $Straight2.left and $Straight3.up and $Straight4.right == true:
+		print("All disconnected")
+	if $Straight.down and $Straight2.right and $Straight3.up and $Straight4.left == true:
+		print("All disconnected")
+	if $Straight.up and $Straight2.left and $Straight3.down and $Straight4.right == true:
+		print("All disconnected")
+	if $Straight.up and $Straight2.right and $Straight3.down and $Straight4.left == true:
+		print("All disconnected")
+	if $Straight.left and $Straight2.up and $Straight3.right and $Straight4.down == true:
+		print("All disconnected")
+	if $Straight.right and $Straight2.up and $Straight3.left and $Straight4.down == true:
+		print("All disconnected")
+	if $Straight.left and $Straight2.down and $Straight3.right and $Straight4.up == true:
+		print("All disconnected")
