8d383f6e18c085cd20382cace2ac4a2f3e36dea9
AmossLee274 <ofahengauekingzton7@gmail.com>
2026-09-18 10:48:46 +1200

    day 2

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -4,22 +4,33 @@ extends CharacterBody2D
 const SPEED = 300.0
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
-		pass
+func _process(delta: float) -> void:
+	if Input.is_action_just_released("left"):
+		$AnimatedSprite2D.play("idle_l")
+	if Input.is_action_just_released("right"):
+		$AnimatedSprite2D.play("idle_r")
+	if Input.is_action_just_released("up"):
+		$AnimatedSprite2D.play("idle_b")
+	if Input.is_action_just_released("down"):
+		$AnimatedSprite2D.play("idle_f")
 	
+	if Input.is_action_pressed("left"):
+		position.x -= 1.2
+		$AnimatedSprite2D.play("run_l")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	if Input.is_action_pressed("right"):
+		position.x += 1.2
+		$AnimatedSprite2D.play("run_r")
+
+
+	if Input.is_action_pressed("up"):
+		position.y -= 1.2
+		$AnimatedSprite2D.play("run_b")
+
+
+	if Input.is_action_pressed("down"):
+		position.y += 1.2
+		$AnimatedSprite2D.play("run_f")
+ 
 	move_and_slide()
